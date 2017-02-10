eac4c07d0a221455af00726b0de4a08c5c989e98
Xijun Li <xijun.li@2359media.com>
2017-02-10 17:08:55 +0800

    Init
    Init

---
+++ MediaCorpSpellingGame/app/src/main/java/com/media2359/mediacorpspellinggame/MainActivity.java
@@ -0,0 +1,13 @@
+package com.media2359.mediacorpspellinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
